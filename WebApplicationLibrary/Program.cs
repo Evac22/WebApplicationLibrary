3e4e2570c8b4c8b06bc3d23a5540f5eff3c5e91c
Vlad <94717630+Evac22@users.noreply.github.com>
2023-05-15 20:10:40 +0300

    Add files via upload

---
+++ WebApplicationLibrary/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace WebApplicationLibrary
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .UseSerilog((hostingContext, loggerConfiguration) =>
+        {
+            loggerConfiguration
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.Console(new CompactJsonFormatter())
+                .WriteTo.File(new CompactJsonFormatter(), "logs/serilog.json", rollingInterval: RollingInterval.Day)
+                .ReadFrom.Configuration(hostingContext.Configuration);
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+            webBuilder.UseUrls("http://localhost:5000", "https://localhost:5001");
+        });
+    }
+}
---
+++ WebApplicationLibrary/Startup.cs
@@ -0,0 +1,61 @@
+﻿
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplicationLibrary.Repositories;
+
+namespace WebApplicationLibrary
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<LibraryContext>(options =>
+                options.UseInMemoryDatabase(databaseName: "Library"));
+
+            services.AddTransient<ILibraryRepository, LibraryRepository>();
+            services.AddScoped<IBookRepository, BookRepository>();
+            services.AddSingleton<ITempDataDictionaryFactory, TempDataDictionaryFactory>();
+
+            services.AddMvc();
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            var context = serviceProvider.GetService<LibraryContext>();
+            var initializer = new LibraryContextInitializer(context);
+            initializer.SeedAsync();
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                name: "books",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapControllerRoute(
+                 name: "books",
+                pattern: "api/[controller]/{action=GetBooks}/{id?}");
+
+                endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "api/{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
