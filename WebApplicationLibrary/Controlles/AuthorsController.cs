322ab863aa75bb0f81b8c24e00923a36e0f4e78d
Vlad <94717630+Evac22@users.noreply.github.com>
2023-05-15 20:04:25 +0300

    Add files via upload

---
+++ WebApplicationLibrary/Controlles/AuthorsController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationLibrary.Models;
+using WebApplicationLibrary.Repositories;
+
+namespace WebApplicationLibrary.Controllers
+{
+    // Этот атрибут указывает на то, что путь URL-адреса для методов в этом контроллере будет начинаться с /api/authors
+    [Route("api/[controller]")]
+    // Атрибут указывает на то, что это контроллер веб-API и обрабатывает запросы RESTful.
+    [ApiController]
+    public class AuthorsController : Controller
+    {
+        //Интерфейс IAuthorRepository определяет контракты для работы с хранилищем данных для авторов, и это поле используется в контроллере для взаимодействия с этим хранилищем данных
+        private readonly IAuthorRepository _authorRepository;
+        
+        // Конструктор
+        public AuthorsController(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Author> GetAuthors()
+        {
+            return _authorRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        //Возвращаю список всех авторов из репозитория
+        public ActionResult<Author> GetAuthor(int id)
+        {
+            var author = _authorRepository.GetById(id);
+
+            if(author == null)
+            {
+                return NotFound();
+            }
+
+            return author;
+        }
+
+        [HttpPost]
+        //Метод CreateAuthor принимает объект автора и добавляет его в репозиторий.
+        //Затем метод возвращает объект CreatedAtAction, который возвращает статус код 201 (создано)
+        //и URL-адрес, который можно использовать для получения созданного объекта.
+        public ActionResult<Author> CreateAuthor(Author author)
+        {
+            _authorRepository.Add(author);
+
+            return CreatedAtAction(nameof(GetAuthor), new { id = author.Id }, author);
+        }
+
+        [HttpPut("{id}")]
+        //Метод UpdateAuthor принимает параметр id и объект автора, который содержит обновленные данные для автора с заданным идентификатором.
+        //Если идентификаторы не совпадают, метод возвращает статус код 400 (неверный запрос).
+        //В противном случае, метод обновляет данные автора в репозитории и возвращает статус код 204 (нет содержимого).
+        public IActionResult UpdateAuthor(int id, Author author)
+        {
+            if (id != author.Id)
+            {
+                return BadRequest();
+            }
+
+            _authorRepository.Update(author);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        //Метод DeleteAuthor принимает параметр id и удаляет автора с заданным идентификатором из репозитория.
+        //Если автор не найден, метод возвращает статус код 404 (не найдено).
+        //В противном случае, метод возвращает статус код 204 (нет содержимого).
+        public IActionResult DeleteAuthor(int id)
+        {
+            var author = _authorRepository.GetById(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _authorRepository.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
---
+++ WebApplicationLibrary/Controlles/BooksController.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication.Data;
+using WebApplicationLibrary.Models;
+using WebApplicationLibrary.Repositories;
+using WebApplicationLibrary.DTO;
+
+namespace WebApplicationLibrary.Controllers
+{
+    //атрибут для определения URL-адреса, по которому будет доступен контроллер
+    [Route("api/[controller]")]
+    //контроллер является контроллером API
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        //это контекст базы данных, который используется для получения данных из базы данных
+        private readonly LibraryContext _context;
+
+        //это интерфейс репозитория для работы с данными книг, который используется для получения и сохранения данных
+        private readonly IBookRepository _bookRepository;
+        public BooksController(LibraryContext context, IBookRepository bookRepository)
+        {
+            _context = context;
+            _bookRepository = bookRepository;
+        }
+
+        // метод для получения всех книг в бд
+        // Get: api/Books
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+        // метод для получения книги по id
+        // Get: api/Books/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return book;
+        }
+        // выполнение условия задачи: Получить все книги. Сортировка по заданному значению (названию или автору)
+        //GET https://{{baseUrl}}/api/books?order=author
+        [HttpGet("GetAllSorted")]
+        public IActionResult GetAllSorted(string order = "title")
+        {
+            try
+            {
+                var books = _bookRepository.GetAllSorted(order);
+                var bookDtos = books.Select(b => new BookDto
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Rating = b.Rating,
+                    ReviewsNumber = b.ReviewsNumber
+                });
+                return Ok(bookDtos);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // метод для обновления информации о книге
+        // PUT: api/Books/5
+        [HttpPut("{id}")]
+        
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpGet("recommended")]
+        public IActionResult GetRecommendedBooks(string genre)
+        {
+            var recommendedBooks = _bookRepository.GetRecommendedBooks(genre);
+
+            var bookDtos = recommendedBooks
+                .OrderByDescending(b => b.Rating)
+                .Take(10)
+                .Where(b => b.ReviewsNumber > 10)
+                .Select(b => new BookDto
+                {
+                    Id = b.Id,
+                    Title = b.Title,
+                    Author = b.Author,
+                    Rating = b.Rating,
+                    ReviewsNumber = b.ReviewsNumber
+                });
+
+            return Ok(bookDtos);
+        }
+
+
+        // метод для добавления новой книги в бд
+        // POST: api/Books
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new {id = book.Id }, book);
+        }
+        // метод для удаления книги из бд по id
+        // DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+        // метод для проверки наличия книги в бд по id
+        private bool BookExists(int id) 
+        { 
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
