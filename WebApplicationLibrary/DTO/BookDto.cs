ad732e22e4c4249742e888e106d7ebf12b557bc2
Vlad <94717630+Evac22@users.noreply.github.com>
2023-05-15 20:04:53 +0300

    Add files via upload

---
+++ WebApplicationLibrary/DTO/BookDto.cs
@@ -0,0 +1,15 @@
+﻿using WebApplicationLibrary.Models;
+
+namespace WebApplicationLibrary.DTO
+{
+    //Класс BookDto (Data Transfer Object) используется для передачи данных веб-приложением между клиентом и сервером
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Author Author { get; set; }
+        public decimal Rating { get; set; }
+        public int ReviewsNumber { get; set; }
+    }
+
+}
